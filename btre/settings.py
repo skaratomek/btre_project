6ad6d6399b35b9574acc3b4e7ef09a07bdba5f41
Tomasz Skaraczynski <skaratomek@gmail.com>
2018-11-25 18:22:14 +0100

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -147,6 +147,11 @@ MESSAGE_TAGS = {
 # Email config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'skaratomek@gmail.com'
-EMAIL_HOST_PASSWORD = 'xwhvudbhbwdqidwp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
